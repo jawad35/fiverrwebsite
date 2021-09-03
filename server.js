bf6a58d33ae508a3ecbfe8d839ae3e7d7f402c8e
jawad ali <024jawadali@gmail.com>
2021-09-03 09:59:34 +0500

    versions 1.0.0

--- server.js
+++ server.js
@@ -51,11 +51,11 @@ app.use("/api/offers", JobOffers);
 app.use("/api/jobs", NewJobs);
 //
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
